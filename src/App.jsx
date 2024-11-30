bfeee1f4e40ef68b4cb84f7c20dd80b4470a990d
Dwi Lestari Amelia <152270443+dwiameliaa@users.noreply.github.com>
2024-11-30 15:46:27 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import GetApiData from "./components/GetApiData";
+import Linktree from "./components/Linktree";
+
+function App() {
+  return (
+    <>
+      <Linktree />
+      {/* <GetApiData /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Linktree.jsx
@@ -0,0 +1,58 @@
+const Linktree = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gradient-to-br from-blue-200 via-purple-100 to-blue-100 p-6">
+      <div className="bg-white rounded-3xl shadow-lg p-10 w-full max-w-md">
+        {/* Header */}
+        <div className="mb-6 text-center">
+          <img
+            src="src/assets/furinaa.jpg"
+            alt="User Avatar"
+            className="rounded-full w-24 h-24 mx-auto border-4 border-blue-300 drop-shadow-md "
+          />
+          <h1 className="text-2xl font-bold mt-4 text-slate-900">Dwi Lestari Amelia</h1>
+          <p className="text-gray-700 font-medium">
+            A computer science student who loves coding and cats. 🐾
+          </p>
+        </div>
+
+        {/* Links */}
+        <div className="space-y-4 text-center">
+          <a
+            href="https://github.com/dwiameliaa"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block w-full py-3 px-6 bg-gradient-to-r from-blue-900 to-blue-500 text-white rounded-full shadow-md hover:shadow-lg hover:scale-105 transform transition duration-300 ease-in-out"
+          >
+            GitHub 🌟
+          </a>
+          <a
+            href="https://www.instagram.com/douiliame/profilecard/?igsh=eW84Mjd5aHFtaDZz"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block w-full py-3 px-6 bg-gradient-to-r from-blue-900 to-blue-500 text-white rounded-full shadow-md hover:shadow-lg hover:scale-105 transform transition duration-300 ease-in-out"
+          >
+            Instagram 📸
+          </a>
+          <a
+            href="https://www.linkedin.com/in/dwi-lestari-amelia-46181532a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block w-full py-3 px-6 bg-gradient-to-r from-blue-900 to-blue-500 text-white rounded-full shadow-md hover:shadow-lg hover:scale-105 transform transition duration-300 ease-in-out"
+          >
+            LinkedIn 💼
+          </a>
+          {/* <a
+            href="https://surabayajs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block w-full py-3 px-6 bg-gradient-to-r from-purple-400 to-pink-400 text-white rounded-full shadow-md hover:shadow-lg hover:scale-105 transform transition duration-300 ease-in-out"
+          >
+            X 🌐
+          </a> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Linktree;
